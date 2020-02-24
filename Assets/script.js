b76f073828e07c08e37b96e1014a7696a48776b6
Brice Boutet <boutetlb@gmail.com>
2020-02-23 21:44:17 -0500

    4 choices buttons moved in html code

--- Assets/script.js
+++ Assets/script.js
@@ -1,119 +1,173 @@
 
+//NOTE: In the code "Answer1" is always the right answer.
+
+var questionsList = [
+    {
+        question: "What is the correct  HTML5 doctype?",
+        answer1: "<!DOCTYPE html>",
+        answer2: "<DOCTYPE html>",
+        answer3: "<!DOCTYPE html5>",
+        answer4: "",
+        answer5: "",
+    },
+   {
+        question: "Which tag start a numbered list?",
+        answer1: "<ol>",
+        answer2: "<al>",
+        answer3: "<ul>",
+        answer4: "<li>",
+        answer5: "",
+    },
+  {
+        question: "Inline elements are normally displayed without starting a new line.",
+        answer1: "True",
+        answer2: "False",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+    },
+  {
+        question: "Which HTML is correct for referencing an external stylesheet",
+        answer1: "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\" />",
+        answer2: "<link src=\"stylesheet\" type=\"text/css\" href\style.css\" />",
+        answer3: "<href=\"stylesheet\" type=\"text/css\" href=\"style.css\" />",
+        answer4: "<src>style.css </src>",
+        answer5: "",
+    },
+
+   {
+        question : "Which CSS property changes the text color of an element?",
+        answer1: "color",
+        answer2: "font-color",
+        answer3: "text-color",
+        answer4: "",
+        answer5: "",
+    },
+    {
+        question : "Is it allowed to use negative values when using padding properties",
+        answer1: "No",
+        answer2: "Yes",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+    },
+   {
+        question: "In which HTML element do we put the JavaScript?",
+        answer1: "<script>",
+        answer2: "<js>",
+        answer3: "<src>",
+        answer4: "",
+        answer5: "",
+    },
+
+    {
+        question: "The external JavaScript file contains the <script> tag.",
+        answer1: "Faste",
+        answer2: "True",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+    },
+
+   {
+        question : "How does a FOR loop start?",
+        answer1: "for(i=0; i<5; i++)",
+        answer1: "for(i=0, i<5, i++)",
+        answer2: "for(i=0 to 5, i++)",
+        answer3: "for(i=0, i<5)",
+        answer4: "",
+        answer5: "",
+    },
+
+    {
+        question : "On how many columns is the Bootstrap grid system based ",
+        answer1: "12",
+        answer2: "10",
+        answer3: "25",
+        answer4: "1024",
+        answer5: "",
+    },
+
+    {
+        question : "Which Bootstrap contextual class indicates a dangerous or negative action? ",
+        answer1: ".text-danger",
+        answer2: ".text-warning",
+        answer3: ".attention",
+        answer4: ".text-stop",
+        answer5: "",
+    },
+
+   {
+        question : "The Bootstrap grid system works across multiple devices.",
+        answer1: "True",
+        answer2: "False",
+        answer3: ".attention",
+        answer4: ".text-stop",
+        answer5: "",
+    },
+
+ {
+        question : "jQuery is a Javascript library.",
+        answer1: "True",
+        answer2: "False",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+    },
+    {
+        question : "Which sign does jQuery use as a shortcut for jQuery?.",
+        answer1: "$",
+        answer2: "#",
+        answer3: "%",
+    },
+
+ {
+        question : "What the following selector: $(\"div\") does?.",
+        answer1: "Select all div elements",
+        answer2: "Select the firt div element",
+
+    },
+
+
+   {
+        question : "What scripting language is jQuery written in?",
+        answer1: "Javascript",
+        answer2: "css",
+        answer3: "C++",
+        answer4: "",
+        answer5: "",
+    }
+
+];
+
+var i= 0;
+
 $(document).ready(function(){
 
-// Starting the quizz
-    $("#startQuiz").click(function(){
+ // Starting the quiz
+ $("#b1, #b2, #b3, #b4").click(function(){
+      //i++;
 
-        //Question text
-        $("h6").html("Question 1");
-
-       //Replace the "Start Quizz button by the 1rst answer choice button"
-       $("#qBtn").html("<button id=\"Ans1\" type=\"button\" class=\"btn btn-success mb-2\">Answer 1</button>");
-       
-       //add 3 more choice buttons to the first one.
-       $("#qBtn").append("<button id=\"Ans2\" type=\"button\" class=\"btn btn-success mb-2\">Answer 2</button>");
-       $("#qBtn").append("<button id=\"Ans3\" type=\"button\" class=\"btn btn-success mb-2\">Answer 3</button>");
-       $("#qBtn").append("<button id=\"Ans4\" type=\"button\" class=\"btn btn-success mb-2\">Answer 4</button>");
+       $("h6").text(questionsList[i].question)
+       $("#b1").text(questionsList[i].answer1);
+       $("#b2").text(questionsList[i].answer2);
+       $("#b3").text(questionsList[i].answer3);
+       $("#b4").text(questionsList[i].answer4);
        
        //Choice list vertical left align
-       $("#qBtn").toggleClass("d-flex flex-column float-left");
+       $("#qBtn").addClass("d-flex flex-column float-left");
+       $("#b1, #b2, #b3, #b4").addClass("text-left")
+
      
+       i++;
+
+       return i;
+
 
     });
+console.log(i)
+
+
+
 });
 
-// Questions and answers objects
-//NOTE: In the code "Answer1" is always the right answer.
-/* var [
-    QuestionOne{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-    QuestionTwo{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-    QuestionTree{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-    QuestionFour{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-
-    QuestionFive{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-
-    QuestionSix{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-
-
-    QuestionSeven{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-
-    QuestionEight{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-
-    QuestionNine{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-
-    QuestionNine{
-        Question : ""
-        Answer1: ""
-        Answer2: ""
-        Answer3: ""
-        Answer4: ""
-        Answer5: ""
-    },
-
-
-
-
-] */
